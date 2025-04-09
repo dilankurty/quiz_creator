2278f62349f1acae995a46b1c3c433bb8a40463b
Kurt <dilanmarinas@gmail.com>
2025-04-09 10:48:08 +0800

    added menu for selection

--- create_quiz.py
+++ create_quiz.py
@@ -1,3 +1,10 @@
 # main menu
-
+def menu():
+    while True:
+        print("welcome to Quizard!")
+        print("[1] Create a quiz")
+        print("[2] Take a quiz")
+        print("[3] Exit")
+        choice = input("Enter your choice: ")
+        
 # create a quiz function
