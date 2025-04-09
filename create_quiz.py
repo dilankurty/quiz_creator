0b69d812cf834cb6ffcd181f9814aa56398569d6
Kurt <dilanmarinas@gmail.com>
2025-04-09 10:53:06 +0800

    added if conditions to direct to selection

--- create_quiz.py
+++ create_quiz.py
@@ -1,10 +1,20 @@
-# main menu
+# main menu system
 def menu():
     while True:
         print("welcome to Quizard!")
         print("[1] Create a quiz")
         print("[2] Take a quiz")
         print("[3] Exit")
+
         choice = input("Enter your choice: ")
-        
-# create a quiz function
+        if choice == "1":
+            create_quiz()
+        elif choice == "2":
+            print("Coming soon...")
+        elif choice == "3":
+            exit()
+        else:
+            print("Invalid input! Please choose between 1 - 3.")
+
+# create a quiz system
+def create_quiz():
